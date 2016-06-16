9594fe5f071aede88ca2f14c905ecaa68b7dc52a
fracpete <fracpete@gmail.com>
2016-06-16 02:30:50 +0000

    evaluator in use can be obtained from a callable actor now

--- adams-spectral-core/src/main/java/adams/flow/transformer/Evaluator.java
+++ adams-spectral-core/src/main/java/adams/flow/transformer/Evaluator.java
@@ -15,15 +15,21 @@
 
 /*
  * Evaluator.java
- * Copyright (C) 2011-2015 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2011-2016 University of Waikato, Hamilton, New Zealand
  */
 
 package adams.flow.transformer;
 
-import adams.flow.core.Token;
+import adams.core.MessageCollection;
+import adams.core.QuickInfoHelper;
+import adams.core.VariableName;
 import adams.data.evaluator.instance.AbstractEvaluator;
 import adams.data.evaluator.instance.NullEvaluator;
+import adams.event.VariableChangeEvent;
 import adams.flow.container.EvaluationContainer;
+import adams.flow.core.CallableActorHelper;
+import adams.flow.core.CallableActorReference;
+import adams.flow.core.Token;
 import weka.core.Instance;
 import weka.core.Instances;
 
@@ -42,13 +48,13 @@ import java.util.Hashtable;
  * - accepts:<br>
  * &nbsp;&nbsp;&nbsp;weka.core.Instances<br>
  * &nbsp;&nbsp;&nbsp;weka.core.Instance<br>
- * &nbsp;&nbsp;&nbsp;knir.flow.container.EvaluationContainer<br>
+ * &nbsp;&nbsp;&nbsp;adams.flow.container.EvaluationContainer<br>
  * - generates:<br>
- * &nbsp;&nbsp;&nbsp;knir.flow.container.EvaluationContainer<br>
+ * &nbsp;&nbsp;&nbsp;adams.flow.container.EvaluationContainer<br>
  * <br><br>
  * Container information:<br>
- * - knir.flow.container.EvaluationContainer: Instance, Instances, Evaluations, Evaluator, Abstention classification, Component<br>
- * - knir.flow.container.EvaluationContainer: Instance, Instances, Evaluations, Evaluator, Abstention classification, Component
+ * - adams.flow.container.EvaluationContainer: Instance, Instances, Evaluations, Evaluator, Abstention classification, Component, ID<br>
+ * - adams.flow.container.EvaluationContainer: Instance, Instances, Evaluations, Evaluator, Abstention classification, Component, ID
  * <br><br>
  <!-- flow-summary-end -->
  *
@@ -81,13 +87,32 @@ import java.util.Hashtable;
  * </pre>
  * 
  * <pre>-silent &lt;boolean&gt; (property: silent)
- * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console.
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing 
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
  * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
  * 
- * <pre>-evaluator &lt;knir.data.evaluator.instance.AbstractEvaluator&gt; (property: evaluator)
+ * <pre>-evaluator &lt;adams.data.evaluator.instance.AbstractEvaluator&gt; (property: evaluator)
  * &nbsp;&nbsp;&nbsp;The Evaluator to train on the input data.
- * &nbsp;&nbsp;&nbsp;default: knir.data.evaluator.instance.NullEvaluator -missing-evaluation NaN
+ * &nbsp;&nbsp;&nbsp;default: adams.data.evaluator.instance.NullEvaluator -missing-evaluation NaN
+ * </pre>
+ * 
+ * <pre>-evaluator-actor &lt;adams.flow.core.CallableActorReference&gt; (property: evaluatorActor)
+ * &nbsp;&nbsp;&nbsp;The callable actor to use for obtaining the evaluator, overrides the evaluator 
+ * &nbsp;&nbsp;&nbsp;option if callable actor available.
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ * <pre>-use-evaluator-reset-variable &lt;boolean&gt; (property: useEvaluatorResetVariable)
+ * &nbsp;&nbsp;&nbsp;If enabled, chnages to the specified variable are monitored in order to 
+ * &nbsp;&nbsp;&nbsp;reset the evaluator, eg when a storage evaluator changed.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-evaluator-reset-variable &lt;adams.core.VariableName&gt; (property: evaluatorResetVariable)
+ * &nbsp;&nbsp;&nbsp;The variable to monitor for changes in order to reset the evaluator, eg 
+ * &nbsp;&nbsp;&nbsp;when a storage evaluator changed.
+ * &nbsp;&nbsp;&nbsp;default: variable
  * </pre>
  * 
  * <pre>-component &lt;java.lang.String&gt; (property: component)
@@ -112,11 +137,23 @@ public class Evaluator
   /** the evaluator. */
   protected AbstractEvaluator m_Evaluator;
 
+  /** the callable actor to get the evaluator from. */
+  protected CallableActorReference m_EvaluatorActor;
+
   /** the evaluator used for training/evaluating. */
   protected AbstractEvaluator m_ActualEvaluator;
 
   /** the name of the component. */
   protected String m_Component;
+
+  /** whether to use a variable to monitor for changes, triggering resets of the evaluator. */
+  protected boolean m_UseEvaluatorResetVariable;
+
+  /** the variable to monitor for changes, triggering resets of the evaluator. */
+  protected VariableName m_EvaluatorResetVariable;
+
+  /** whether we need to reset the evaluator. */
+  protected boolean m_ResetEvaluator;
 
   /**
    * Returns a string describing the object.
@@ -141,6 +178,18 @@ public class Evaluator
     m_OptionManager.add(
       "evaluator", "evaluator",
       new NullEvaluator());
+
+    m_OptionManager.add(
+      "evaluator-actor", "evaluatorActor",
+      new CallableActorReference());
+
+    m_OptionManager.add(
+      "use-evaluator-reset-variable", "useEvaluatorResetVariable",
+      false);
+
+    m_OptionManager.add(
+      "evaluator-reset-variable", "evaluatorResetVariable",
+      new VariableName());
 
     m_OptionManager.add(
       "component", "component",
@@ -174,6 +223,103 @@ public class Evaluator
    */
   public String evaluatorTipText() {
     return "The Evaluator to train on the input data.";
+  }
+
+  /**
+   * Sets the callable actor to obtain the evaluator.
+   * Overrides the evaluator option if callable exists.
+   *
+   * @param value	the actor reference
+   */
+  public void setEvaluatorActor(CallableActorReference value) {
+    m_EvaluatorActor = value;
+    reset();
+  }
+
+  /**
+   * Returns the callable actor to obtain the evaluator from.
+   * Overrides the evaluator option if callable exists.
+   *
+   * @return		the actor reference
+   */
+  public CallableActorReference getEvaluatorActor() {
+    return m_EvaluatorActor;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String evaluatorActorTipText() {
+    return
+      "The callable actor to use for obtaining the evaluator, overrides the "
+	+ "evaluator option if callable actor available.";
+  }
+
+  /**
+   * Sets the whether to use a variable to monitor for changes in order
+   * to reset the evaluator.
+   *
+   * @param value	true if to use monitor variable
+   */
+  public void setUseEvaluatorResetVariable(boolean value) {
+    m_UseEvaluatorResetVariable = value;
+    reset();
+  }
+
+  /**
+   * Returns the whether to use a variable to monitor for changes in order
+   * to reset the evaluator.
+   *
+   * @return		true if to use monitor variable
+   */
+  public boolean getUseEvaluatorResetVariable() {
+    return m_UseEvaluatorResetVariable;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String useEvaluatorResetVariableTipText() {
+    return
+        "If enabled, chnages to the specified variable are monitored in order "
+	  + "to reset the evaluator, eg when a storage evaluator changed.";
+  }
+
+  /**
+   * Sets the variable to monitor for changes in order to reset the evaluator.
+   *
+   * @param value	the variable
+   */
+  public void setEvaluatorResetVariable(VariableName value) {
+    m_EvaluatorResetVariable = value;
+    reset();
+  }
+
+  /**
+   * Returns the variable to monitor for changes in order to reset the evaluator.
+   *
+   * @return		the variable
+   */
+  public VariableName getEvaluatorResetVariable() {
+    return m_EvaluatorResetVariable;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String evaluatorResetVariableTipText() {
+    return
+        "The variable to monitor for changes in order to reset the evaluator, eg "
+	  + "when a storage evaluator changed.";
   }
 
   /**
@@ -214,6 +360,7 @@ public class Evaluator
   public String getQuickInfo() {
     String	result;
     String	variable;
+    String	value;
 
     result   = "evaluator: ";
     variable = getOptionManager().getVariableForProperty("evaluator");
@@ -221,6 +368,9 @@ public class Evaluator
       result += variable;
     else
       result += m_Evaluator.getClass().getSimpleName();
+    value  = QuickInfoHelper.toString(this, "evaluatorResetVariable", (m_UseEvaluatorResetVariable ? "reset: " + m_EvaluatorResetVariable : ""));
+    if (value != null)
+      result += ", " + value;
 
     return result;
   }
@@ -280,7 +430,7 @@ public class Evaluator
   /**
    * Returns the class that the consumer accepts.
    *
-   * @return		<!-- flow-accepts-start -->weka.core.Instances.class, weka.core.Instance.class, knir.flow.container.EvaluationContainer.class<!-- flow-accepts-end -->
+   * @return		<!-- flow-accepts-start -->weka.core.Instances.class, weka.core.Instance.class, adams.flow.container.EvaluationContainer.class<!-- flow-accepts-end -->
    */
   @Override
   public Class[] accepts() {
@@ -290,11 +440,56 @@ public class Evaluator
   /**
    * Returns the class of objects that it generates.
    *
-   * @return		<!-- flow-generates-start -->knir.flow.container.EvaluationContainer.class<!-- flow-generates-end -->
+   * @return		<!-- flow-generates-start -->adams.flow.container.EvaluationContainer.class<!-- flow-generates-end -->
    */
   @Override
   public Class[] generates() {
     return new Class[]{EvaluationContainer.class};
+  }
+
+  /**
+   * Loads the evaluator from the evaluator file.
+   *
+   * @return		null if everything worked, otherwise an error message
+   */
+  protected String setUpEvaluator() {
+    String		result;
+    Object		obj;
+    MessageCollection errors;
+
+    result = null;
+
+    // obtain evaluator from callable actor
+    try {
+      errors = new MessageCollection();
+      obj    = CallableActorHelper.getSetup(null, m_EvaluatorActor, this, errors);
+      if (obj != null)
+	m_ActualEvaluator = (AbstractEvaluator) obj;
+    }
+    catch (Exception e) {
+      m_ActualEvaluator = null;
+      result = handleException("Failed to obtain evaluator from callable actor '" + m_EvaluatorActor + "': ", e);
+    }
+
+    // use defined evaluator
+    if (m_ActualEvaluator == null)
+      m_ActualEvaluator = m_Evaluator.shallowCopy();
+
+    m_ResetEvaluator = false;
+
+    return result;
+  }
+
+  /**
+   * Gets triggered when a variable changed (added, modified, removed).
+   *
+   * @param e		the event
+   */
+  @Override
+  public void variableChanged(VariableChangeEvent e) {
+    super.variableChanged(e);
+    if (e.getName().equals(m_EvaluatorResetVariable.getValue()))
+      m_ResetEvaluator = true;
   }
 
   /**
@@ -314,69 +509,71 @@ public class Evaluator
 
     result = null;
 
-    if (m_ActualEvaluator == null)
-      m_ActualEvaluator = m_Evaluator.shallowCopy();
+    if ((m_ActualEvaluator == null) || m_ResetEvaluator)
+      result = setUpEvaluator();
 
-    try {
-      // get data
-      data  = null;
-      inst  = null;
-      cont  = null;
-      evals = new Hashtable<String,Object>();
-      if (m_InputToken.getPayload() instanceof EvaluationContainer) {
-	cont = (EvaluationContainer) m_InputToken.getPayload();
-	if (cont.hasValue(EvaluationContainer.VALUE_INSTANCES))
-	  data = (Instances) cont.getValue(EvaluationContainer.VALUE_INSTANCES);
-	else if (cont.hasValue(EvaluationContainer.VALUE_INSTANCE))
-	  inst = (Instance) cont.getValue(EvaluationContainer.VALUE_INSTANCE);
-	evals = (Hashtable<String,Object>) cont.getValue(EvaluationContainer.VALUE_EVALUATIONS);
-      }
-      else if (m_InputToken.getPayload() instanceof Instances) {
-	data = (Instances) m_InputToken.getPayload();
-      }
-      else if (m_InputToken.getPayload() instanceof Instance) {
-	inst = (Instance) m_InputToken.getPayload();
-      }
-      else {
-	throw new IllegalArgumentException(
+    if (result == null) {
+      try {
+	// get data
+	data = null;
+	inst = null;
+	cont = null;
+	evals = new Hashtable<>();
+	if (m_InputToken.getPayload() instanceof EvaluationContainer) {
+	  cont = (EvaluationContainer) m_InputToken.getPayload();
+	  if (cont.hasValue(EvaluationContainer.VALUE_INSTANCES))
+	    data = (Instances) cont.getValue(EvaluationContainer.VALUE_INSTANCES);
+	  else if (cont.hasValue(EvaluationContainer.VALUE_INSTANCE))
+	    inst = (Instance) cont.getValue(EvaluationContainer.VALUE_INSTANCE);
+	  evals = (Hashtable<String, Object>) cont.getValue(EvaluationContainer.VALUE_EVALUATIONS);
+	}
+	else if (m_InputToken.getPayload() instanceof Instances) {
+	  data = (Instances) m_InputToken.getPayload();
+	}
+	else if (m_InputToken.getPayload() instanceof Instance) {
+	  inst = (Instance) m_InputToken.getPayload();
+	}
+	else {
+	  throw new IllegalArgumentException(
 	    "Unhandled input: " + m_InputToken.getPayload().getClass().getName());
-      }
+	}
 
-      // process data
-      eval = null;
-      if (data != null) {
-	m_ActualEvaluator.build(data);
-      }
-      else if (inst != null) {
-	eval = m_ActualEvaluator.evaluate(inst);
-      }
+	// process data
+	eval = null;
+	if (data != null) {
+	  m_ActualEvaluator.build(data);
+	}
+	else if (inst != null) {
+	  eval = m_ActualEvaluator.evaluate(inst);
+	}
 
-      // generate output
-      if (cont != null)
-	newCont = (EvaluationContainer) cont.getClone();
-      else
-	newCont = new EvaluationContainer();
-      newCont.setValue(EvaluationContainer.VALUE_EVALUATOR, m_ActualEvaluator);
-      if (data != null)
-	newCont.setValue(EvaluationContainer.VALUE_INSTANCES, data);
-      if (inst != null)
-	newCont.setValue(EvaluationContainer.VALUE_INSTANCE, inst);
-      if (eval != null) {
-        if (eval.size() == 1) {
-          evals.put(getName(), eval.get(AbstractEvaluator.DEFAULT_METRIC));
-        }
-        else {
-          for (String name: eval.keySet())
-            evals.put(getName() + "." + name, eval.get(name));
-        }
+	// generate output
+	if (cont != null)
+	  newCont = (EvaluationContainer) cont.getClone();
+	else
+	  newCont = new EvaluationContainer();
+	newCont.setValue(EvaluationContainer.VALUE_EVALUATOR, m_ActualEvaluator);
+	if (data != null)
+	  newCont.setValue(EvaluationContainer.VALUE_INSTANCES, data);
+	if (inst != null)
+	  newCont.setValue(EvaluationContainer.VALUE_INSTANCE, inst);
+	if (eval != null) {
+	  if (eval.size() == 1) {
+	    evals.put(getName(), eval.get(AbstractEvaluator.DEFAULT_METRIC));
+	  }
+	  else {
+	    for (String name : eval.keySet())
+	      evals.put(getName() + "." + name, eval.get(name));
+	  }
+	}
+	newCont.setValue(EvaluationContainer.VALUE_EVALUATIONS, evals);
+	newCont.setValue(EvaluationContainer.VALUE_COMPONENT, m_Component);
+	m_OutputToken = new Token(newCont);
       }
-      newCont.setValue(EvaluationContainer.VALUE_EVALUATIONS, evals);
-      newCont.setValue(EvaluationContainer.VALUE_COMPONENT, m_Component);
-      m_OutputToken = new Token(newCont);
-    }
-    catch (Exception e) {
-      m_OutputToken = null;
-      result = handleException("Failed to evaluate: " + m_InputToken.getPayload(), e);
+      catch (Exception e) {
+	m_OutputToken = null;
+	result = handleException("Failed to evaluate: " + m_InputToken.getPayload(), e);
+      }
     }
 
     return result;
