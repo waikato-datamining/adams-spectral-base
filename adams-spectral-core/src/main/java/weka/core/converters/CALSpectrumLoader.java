578f8be4ec374a5fafade7e403dc470d68c36cba
fracpete <fracpete@gmail.com>
2016-10-18 23:04:34 +0000

    Weka file loader for FOSS CAL files

---
+++ adams-spectral-core/src/main/java/weka/core/converters/CALSpectrumLoader.java
@@ -0,0 +1,206 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * CALSpectrumLoader.java
+ * Copyright (C) 2016 University of Waikato, Hamilton, NZ
+ */
+
+package weka.core.converters;
+
+import adams.core.io.FileUtils;
+import adams.core.io.PlaceholderFile;
+import adams.data.instances.SimpleInstanceGenerator;
+import adams.data.io.input.CALSpectrumReader;
+import adams.data.io.input.foss.FossHelper;
+import adams.data.report.DataType;
+import adams.data.report.Field;
+import adams.data.spectrum.Spectrum;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.RevisionUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Vector;
+
+/**
+ * Loads a CAL file.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class CALSpectrumLoader
+  extends AbstractFileLoader {
+
+  private static final long serialVersionUID = 3251733079008628734L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  public String globalInfo() {
+    return new CALSpectrumReader().globalInfo();
+  }
+
+  /**
+   * Get the file extension used for this type of file
+   *
+   * @return the file extension
+   */
+  @Override
+  public String getFileExtension() {
+    return getFileExtensions()[0];
+  }
+
+  /**
+   * Gets all the file extensions used for this type of file
+   *
+   * @return the file extensions
+   */
+  @Override
+  public String[] getFileExtensions() {
+    return new CALSpectrumReader().getFormatExtensions();
+  }
+
+  /**
+   * Get a one line description of the type of file
+   *
+   * @return a description of the file type
+   */
+  @Override
+  public String getFileDescription() {
+    return new CALSpectrumReader().getFormatDescription();
+  }
+
+  /**
+   * Does nothing.
+   *
+   * @param input the input stream
+   * @throws IOException never
+   */
+  @Override
+  public void setSource(InputStream input) throws IOException {
+    // ignored
+  }
+
+  /**
+   * Determines and returns (if possible) the structure (internally the header)
+   * of the data set as an empty set of instances.
+   * <br>
+   * Inefficient, as it just returns an empty dataset derived from
+   * a call to {@link #getDataSet()}.
+   *
+   * @return the structure of the data set as an empty set of Instances
+   * @throws IOException if there is no source or parsing fails
+   */
+  @Override
+  public Instances getStructure() throws IOException {
+    return new Instances(getDataSet(), 0);
+  }
+
+  /**
+   * Return the full data set. If the structure hasn't yet been determined by a
+   * call to getStructure then the method should do so before processing the
+   * rest of the data set.
+   *
+   * @return the full data set as an Instances object
+   * @throws IOException if there is an error during parsing or if
+   *           getNextInstance has been called on this source (either
+   *           incremental or batch loading can be used, not both).
+   */
+  @Override
+  public Instances getDataSet() throws IOException {
+    Instances			result;
+    File			file;
+    CALSpectrumReader		reader;
+    List<Spectrum>		spectra;
+    FossHelper 			fh;
+    Vector<String> 		refs;
+    Field[]			fields;
+    int				i;
+    SimpleInstanceGenerator	generator;
+    Instance			inst;
+
+    file = retrieveFile();
+    if (file == null)
+      throw new IOException("No file name provided!");
+
+    reader = new CALSpectrumReader();
+    reader.setInput(new PlaceholderFile(file));
+    spectra = reader.read();
+    if (spectra == null)
+      throw new IOException("Failed to read spectra from: " + file);
+
+    fh   = new FossHelper(FileUtils.loadFromBinaryFile(file.getAbsoluteFile()));
+    refs = fh.getReferenceNames();
+    fields = new Field[refs.size() - 1];
+    for (i = 0; i < refs.size() - 1; i++)
+      fields[i] = new Field(refs.get(i).toLowerCase(), DataType.NUMERIC);
+    generator = new SimpleInstanceGenerator();
+    generator.setAddSampleID(true);
+    generator.setAdditionalFields(fields);
+    generator.setNoAdditionalFieldsPrefix(true);
+    if (refs.size() > 0)
+      generator.setField(new Field(refs.get(refs.size() - 1).toLowerCase(), DataType.NUMERIC));
+    else
+      generator.setField(new Field("dummyclass", DataType.NUMERIC));
+    result = null;
+    for (Spectrum sp: spectra) {
+      inst = generator.generate(sp);
+      if (result == null) {
+	result = new Instances(inst.dataset(), spectra.size());
+	result.setRelationName(FileUtils.replaceExtension(file.getName(), ""));
+      }
+      result.add(inst);
+    }
+
+    return result;
+  }
+
+  /**
+   * CALSpectrumLoader is unable to process a data set incrementally.
+   *
+   * @param structure		ignored
+   * @return 			never returns without throwing an exception
+   * @throws IOException 	always. CALSpectrumLoader is unable to process a
+   * 				data set incrementally.
+   */
+  public Instance getNextInstance(Instances structure) throws IOException {
+    throw new IOException("CALSpectrumLoader can't read data sets incrementally.");
+  }
+
+  /**
+   * Returns the revision string.
+   *
+   * @return the revision
+   */
+  @Override
+  public String getRevision() {
+    return RevisionUtils.extract("$Revision: 11506 $");
+  }
+
+  /**
+   * Main method.
+   *
+   * @param args should contain the name of an input file.
+   */
+  public static void main(String[] args) {
+    runFileLoader(new CALSpectrumLoader(), args);
+  }
+}
---
+++ adams-spectral-core/src/test/java/weka/core/converters/CALSpectrumLoaderTest.java
@@ -0,0 +1,107 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * CALSpectrumLoaderTest.java
+ * Copyright (C) 2016 University of Waikato, Hamilton, NZ
+ */
+
+package weka.core.converters;
+
+import adams.core.Utils;
+import adams.env.Environment;
+import adams.test.AbstractTestHelper;
+import adams.test.AdamsTestCase;
+import adams.test.TestHelper;
+import adams.test.TmpFile;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSink;
+
+import java.io.File;
+
+/**
+ * Tests the CALSpectrumLoader.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class CALSpectrumLoaderTest
+  extends AdamsTestCase {
+
+  /**
+   * Constructs the test case. Called by subclasses.
+   *
+   * @param name the name of the test
+   */
+  public CALSpectrumLoaderTest(String name) {
+    super(name);
+  }
+
+  /**
+   * Performs a regression test, comparing against previously generated output.
+   */
+  public void testRegression() {
+    TmpFile	data;
+    TmpFile	arff;
+    String	regression;
+
+    m_TestHelper.copyResourceToTmp("CALK10.CAL");
+    data = new TmpFile("CALK10.CAL");
+    arff = new TmpFile("CALK10.arff");
+    try {
+      CALSpectrumLoader loader = new CALSpectrumLoader();
+      loader.setFile(data.getAbsoluteFile());
+      Instances inst = loader.getDataSet();
+      DataSink.write(arff.getAbsolutePath(), inst);
+      regression = m_Regression.compare(new File[]{arff});
+      assertNull("Output differs:\n" + regression, regression);
+    }
+    catch (Exception e) {
+      fail("Failed regression test!\n" + Utils.throwableToString(e));
+    }
+    m_TestHelper.deleteFileFromTmp("CALK10.CAL");
+  }
+
+  /**
+   * Returns the test helper class to use.
+   *
+   * @return		the helper class instance
+   */
+  @Override
+  protected AbstractTestHelper newTestHelper() {
+    return new TestHelper(this, "weka/core/converters/data");
+  }
+
+  /**
+   * Returns a test suite.
+   *
+   * @return		the test suite
+   */
+  public static Test suite() {
+    return new TestSuite(CALSpectrumLoaderTest.class);
+  }
+
+  /**
+   * Runs the test from commandline.
+   *
+   * @param args	ignored
+   */
+  public static void main(String[] args) {
+    Environment.setEnvironmentClass(Environment.class);
+    runTest(suite());
+  }
+}
