32bd117d00a983e5b68fc6faac2733cacbf3b8cc
fracpete <fracpete@gmail.com>
2017-07-03 03:08:08 +0000

    updated javadoc

--- adams-spectral-3way-core/src/main/java/adams/data/io/input/SimpleEEMReader.java
+++ adams-spectral-3way-core/src/main/java/adams/data/io/input/SimpleEEMReader.java
@@ -27,7 +27,51 @@ import adams.data.threeway.LevelTwoPoint;
 import adams.data.threeway.ThreeWayData;
 
 /**
+ <!-- globalinfo-start -->
  * Reads EEM data in spreadsheet format (tab-separated columns).
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ * <pre>-input &lt;adams.core.io.PlaceholderFile&gt; (property: input)
+ * &nbsp;&nbsp;&nbsp;The file to read and turn into a container.
+ * &nbsp;&nbsp;&nbsp;default: ${CWD}
+ * </pre>
+ * 
+ * <pre>-create-dummy-report &lt;boolean&gt; (property: createDummyReport)
+ * &nbsp;&nbsp;&nbsp;If true, then a dummy report is created if none present.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-instrument &lt;java.lang.String&gt; (property: instrument)
+ * &nbsp;&nbsp;&nbsp;The name of the instrument that generated the spectra (if not already present 
+ * &nbsp;&nbsp;&nbsp;in data).
+ * &nbsp;&nbsp;&nbsp;default: unknown
+ * </pre>
+ * 
+ * <pre>-format &lt;java.lang.String&gt; (property: format)
+ * &nbsp;&nbsp;&nbsp;The data format string.
+ * &nbsp;&nbsp;&nbsp;default: EEM
+ * </pre>
+ * 
+ * <pre>-keep-format &lt;boolean&gt; (property: keepFormat)
+ * &nbsp;&nbsp;&nbsp;If enabled the format obtained from the file is not replaced by the format 
+ * &nbsp;&nbsp;&nbsp;defined here.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-use-absolute-source &lt;boolean&gt; (property: useAbsoluteSource)
+ * &nbsp;&nbsp;&nbsp;If enabled the source report field stores the absolute file name rather 
+ * &nbsp;&nbsp;&nbsp;than just the name.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ <!-- options-end -->
  *
  * @author FracPete (fracpete at waikato dot ac dot nz)
  * @version $Revision$
