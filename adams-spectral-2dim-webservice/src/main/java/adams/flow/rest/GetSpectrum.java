72c03117e8fe581572e4c97ca4f4cd163b903aa0
Peter Reutemann <fracpete@gmail.com>
2018-01-24 10:59:34 +1300

    return 500 Server Error now in case of problems

--- adams-spectral-2dim-webservice/src/main/java/adams/flow/rest/GetSpectrum.java
+++ adams-spectral-2dim-webservice/src/main/java/adams/flow/rest/GetSpectrum.java
@@ -24,12 +24,12 @@ import adams.data.conversion.SpectrumToJson;
 import adams.data.spectrum.JsonUtils;
 import adams.data.spectrum.Spectrum;
 import adams.db.SpectrumT;
-import com.google.gson.JsonObject;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 /**
  * REST plugin for retrieving spectra.
@@ -63,30 +63,25 @@ public class GetSpectrum
   @GET
   @Path("/spectrum/get/{id}/{format}")
   @Produces("text/json")
-  public String get(@PathParam("id") String id, @PathParam("format") String format) {
+  public Response get(@PathParam("id") String id, @PathParam("format") String format) {
     Spectrum 		sp;
-    JsonObject 		json;
     SpectrumToJson	conv;
     String		msg;
 
     initDatabase();
     sp = SpectrumT.getSingleton(m_DatabaseConnection).load(id, format);
     if (sp == null) {
-      json = new JsonObject();
-      json.addProperty("message", "not found");
-      return json.toString();
+      return Response.serverError().entity("Spectrum not found: " + id + "/" + format).build();
     }
     else {
       conv = new SpectrumToJson();
       conv.setInput(sp);
       msg = conv.convert();
       if (msg == null) {
-        return (String) conv.getOutput();
+        return Response.ok(conv.getOutput()).build();
       }
       else {
-	json = new JsonObject();
-	json.addProperty("message", msg);
-	return json.toString();
+	return Response.serverError().entity("Failed to convert spectrum into json: " + id + "/" + format).build();
       }
     }
   }
