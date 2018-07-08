ac48ccb9cad06bf5b1c77ef333894da9ec2b4ead
Peter Reutemann <fracpete@gmail.com>
2018-07-09 10:56:11 +1200

    added option to only output every nth point (default = 1)

--- adams-spectral-3way-core/src/main/java/adams/data/threewayfeatures/Flatten.java
+++ adams-spectral-3way-core/src/main/java/adams/data/threewayfeatures/Flatten.java
@@ -39,6 +39,9 @@ public class Flatten
 
   private static final long serialVersionUID = 1731792416553098298L;
 
+  /** the n-th point to use. */
+  protected int m_NthPoint;
+
   /**
    * Returns a string describing the object.
    *
@@ -47,6 +50,49 @@ public class Flatten
   @Override
   public String globalInfo() {
     return "Simply flattens the 3-way data structure (x -> y -> z).";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+      "nth", "nthPoint",
+      1, 1, null);
+  }
+
+  /**
+   * Sets the nth point setting.
+   *
+   * @param value 	the nth point to use
+   */
+  public void setNthPoint(int value) {
+    if (getOptionManager().isValid("nthPoint", value)) {
+      m_NthPoint = value;
+      reset();
+    }
+  }
+
+  /**
+   * Returns the nth point setting.
+   *
+   * @return 		the nth point
+   */
+  public int getNthPoint() {
+    return m_NthPoint;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String nthPointTipText() {
+    return "Only every n-th point will be output.";
   }
 
   /**
@@ -66,8 +112,10 @@ public class Flatten
       count += l1.size();
 
     result = new HeaderDefinition();
-    for (i = 0; i < count; i++)
-      result.add("Value-" + (i+1), DataType.NUMERIC);
+    for (i = 0; i < count; i++) {
+      if (i % m_NthPoint == 0)
+	result.add("Value-" + (i + 1), DataType.NUMERIC);
+    }
 
     return result;
   }
@@ -81,13 +129,18 @@ public class Flatten
   @Override
   public List<Object>[] generateRows(ThreeWayData data) {
     List<Object>[]	result;
+    int			i;
 
     result = new List[1];
     result[0] = new ArrayList<>();
 
+    i = 0;
     for (L1Point l1: data) {
-      for (L2Point l2: l1)
-	result[0].add(l2.getData());
+      for (L2Point l2: l1) {
+	if (i % m_NthPoint == 0)
+	  result[0].add(l2.getData());
+	i++;
+      }
     }
 
     return result;
