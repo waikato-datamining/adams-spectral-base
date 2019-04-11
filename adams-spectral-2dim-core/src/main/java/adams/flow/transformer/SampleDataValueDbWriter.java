624a82d5b74af3cc61e915bd4a1166c65e1e8184
Peter Reutemann <fracpete@gmail.com>
2019-04-12 10:10:16 +1200

    added SampleDataValueDbWriter for writing specific sample data values to the database

---
+++ adams-spectral-2dim-core/src/main/java/adams/flow/transformer/SampleDataValueDbWriter.java
@@ -0,0 +1,231 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * SampleDataValueDbWriter.java
+ * Copyright (C) 2019 University of Waikato, Hamilton, NZ
+ */
+
+package adams.flow.transformer;
+
+import adams.data.report.AbstractField;
+import adams.data.report.Field;
+import adams.data.report.Report;
+import adams.data.report.ReportHandler;
+import adams.data.sampledata.SampleData;
+import adams.db.AbstractDatabaseConnection;
+import adams.db.DatabaseConnection;
+import adams.db.ReportProviderByID;
+import adams.db.SampleDataF;
+import adams.flow.core.ActorUtils;
+
+/**
+ <!-- globalinfo-start -->
+ *
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- flow-summary-start -->
+ * Input&#47;output:<br>
+ * - accepts:<br>
+ * &nbsp;&nbsp;&nbsp;adams.data.report.Report<br>
+ * &nbsp;&nbsp;&nbsp;adams.data.sampledata.SampleData<br>
+ * &nbsp;&nbsp;&nbsp;adams.data.report.ReportHandler<br>
+ * - generates:<br>
+ * &nbsp;&nbsp;&nbsp;adams.data.report.Report<br>
+ * &nbsp;&nbsp;&nbsp;adams.data.sampledata.SampleData<br>
+ * &nbsp;&nbsp;&nbsp;adams.data.report.ReportHandler<br>
+ * <br><br>
+ <!-- flow-summary-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ *
+ * <pre>-name &lt;java.lang.String&gt; (property: name)
+ * &nbsp;&nbsp;&nbsp;The name of the actor.
+ * &nbsp;&nbsp;&nbsp;default: SampleDataValueDbWriter
+ * </pre>
+ *
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
+ * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
+ * &nbsp;&nbsp;&nbsp;default:
+ * </pre>
+ *
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded
+ * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical
+ * &nbsp;&nbsp;&nbsp;actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-field &lt;adams.data.report.Field&gt; [-field ...] (property: fields)
+ * &nbsp;&nbsp;&nbsp;The fields to store in the sampledata table.
+ * &nbsp;&nbsp;&nbsp;default:
+ * </pre>
+ *
+ <!-- options-end -->
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class SampleDataValueDbWriter
+  extends AbstractReportValueDbWriter<SampleData> {
+
+  private static final long serialVersionUID = 1688669671731516545L;
+
+  @Override
+  public String globalInfo() {
+    return null;
+  }
+
+  /**
+   * Returns the default fields for the option.
+   *
+   * @return		the default fields
+   */
+  @Override
+  protected AbstractField[] getDefaultFields() {
+    return new Field[0];
+  }
+
+  /**
+   * Sets the fields to store in the database.
+   *
+   * @param value	the fields
+   */
+  public void setFields(Field[] value) {
+    m_Fields = value;
+    reset();
+  }
+
+  /**
+   * Returns the fields to store in the database.
+   *
+   * @return		the fields
+   */
+  public Field[] getFields() {
+    return m_Fields;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  @Override
+  public String fieldsTipText() {
+    return "The fields to store in the sampledata table.";
+  }
+
+  /**
+   * Returns the class that the consumer accepts.
+   *
+   * @return		the report class
+   */
+  @Override
+  public Class[] accepts() {
+    return new Class[]{Report.class, SampleData.class, ReportHandler.class};
+  }
+
+  /**
+   * Returns the class of objects that it generates.
+   *
+   * @return		the report class
+   */
+  @Override
+  public Class[] generates() {
+    return new Class[]{Report.class, SampleData.class, ReportHandler.class};
+  }
+
+  /**
+   * Returns the report provider to use for removing the
+   *
+   * @return		the provider
+   */
+  @Override
+  protected ReportProviderByID<SampleData> getReportProvider() {
+    return SampleDataF.getSingleton(m_DatabaseConnection);
+  }
+
+  /**
+   * Returns the default database connection.
+   *
+   * @return 		the default database connection
+   */
+  @Override
+  protected AbstractDatabaseConnection getDefaultDatabaseConnection() {
+    return DatabaseConnection.getSingleton();
+  }
+
+  /**
+   * Determines the database connection in the flow.
+   *
+   * @return		the database connection to use
+   */
+  @Override
+  protected AbstractDatabaseConnection getDatabaseConnection() {
+    return ActorUtils.getDatabaseConnection(
+      this,
+      adams.flow.standalone.DatabaseConnectionProvider.class,
+      getDefaultDatabaseConnection());
+  }
+
+  /**
+   * Extracts the ID from the report.
+   *
+   * @param report	the report to extract the ID from
+   * @return		the ID
+   */
+  @Override
+  protected String extractID(SampleData report) {
+    return report.getID();
+  }
+
+  /**
+   * Generates a subset of the report, which only contains the specified fields.
+   *
+   * @param report	the report to process
+   * @return		the subset report
+   */
+  @Override
+  protected SampleData extractSubset(SampleData report) {
+    SampleData	result;
+
+    result = new SampleData();
+    result.setID(report.getID());
+    for (Field field: m_Fields) {
+      result.addField(field);
+      if (report.hasValue(field))
+	result.setValue(field, report.getValue(field));
+    }
+
+    return result;
+  }
+}
